c78d26aa9295ef49d0900c3080b47430c06617f1
Eszter <szteran@gmail.com>
2023-11-22 13:00:21 +0100

    done

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector("#root").insertAdjacentHTML("beforeend", `
+    <form>
+      <h1>Visszajelzés</h1>
+
+      <input type="text" placeholder="Tárgy">
+
+      <textarea placeholder="Megjegyzés"></textarea>
+
+      <div> 
+        <input type="checkbox">
+        <p>Elolvastam és elfogadom az <span>Adatkezelési Tájékoztatót.</span></p>
+      </div>
+
+      <div>
+        <input type="checkbox">
+        <p>Szeretnék hírlevelet kapni.</p>
+      </div>
+
+      <button>mentés</button>
+    </form>
+    `)
